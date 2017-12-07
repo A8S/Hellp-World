8ddbb1e938cbd1f1ffec19ce061f23a7a35c25cc
A8S <33037679+A8S@users.noreply.github.com>
2017-12-07 16:19:38 +0530

    Create Array Summission.py

---
+++ Array Summission.py
@@ -0,0 +1,12 @@
+#Question -Add elements of an array ,the user should provide the array.
+numArray = map(int, input().split()) # Get the input
+
+
+sum_integer = 0
+
+for number in numArray :
+    sum_integer=number + sum_integer
+
+
+
+print(sum_integer) 
