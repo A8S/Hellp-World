8e880a7e70f2e3f743be97b84b3af0940108be47
A8S <33037679+A8S@users.noreply.github.com>
2017-12-07 19:57:58 +0530

    Update Array Summission.py

--- Array Summission.py
+++ Array Summission.py
@@ -1,11 +1,11 @@
 #Question -Add elements of an array ,the user should provide the array.
-numArray = map(int, input().split()) # Get the input
+numArray = map(int, input().split())# this statement is used for getting the array from the user
 
 
 sum_integer = 0
 
-for number in numArray :
-    sum_integer=number + sum_integer
+for number in numArray :#loop
+    sum_integer=number + sum_integer#increments the value of sum_integer
 
 
 
